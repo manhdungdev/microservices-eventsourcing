96a51cd9bad53a18c29eaf07d150add9b7629956
Hoang Manh Dung <manhdung18102003@gmail.com>
2024-11-11 21:50:44 +0700

    API swagger

--- employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/command/controller/EmployeeCommandController.java
@@ -5,6 +5,7 @@ import com.ltfullstack.employeeservice.command.command.DeleteEmployeeCommand;
 import com.ltfullstack.employeeservice.command.command.UpdateEmployeeCommand;
 import com.ltfullstack.employeeservice.command.model.CreateEmployeeModel;
 import com.ltfullstack.employeeservice.command.model.UpdateEmployeeModel;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class EmployeeCommandController {
         return commandGateway.sendAndWait(command);
     }
 
+    @Hidden
     @DeleteMapping("/{employeeId}")
     public String deleteEmployee(@PathVariable String employeeId){
         DeleteEmployeeCommand command = new DeleteEmployeeCommand(employeeId);
---
+++ employeeservice/employeeservice/src/main/java/com/ltfullstack/employeeservice/query/queries/GetAllEmployeeQuery.java
@@ -0,0 +1,11 @@
+package com.ltfullstack.employeeservice.query.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllEmployeeQuery {
+    private Boolean isDisciplined;
+}
